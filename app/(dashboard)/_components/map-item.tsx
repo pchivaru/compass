e21b63b51c013bb27b3e43032efc078b7592ba07
pchivaru <127643419+pchivaru@users.noreply.github.com>
2024-11-23 12:03:19 +0100

    Insert ArcGis DEMO

--- app/(dashboard)/_components/map-item.tsx
+++ app/(dashboard)/_components/map-item.tsx
@@ -4,29 +4,36 @@ import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import './map.css';
 
-export default function Map() {
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const lng = -0.44186;
-    const lat = 39.41974;
-    const zoom = 14;
-    const API_KEY = 'qUbbRpXJLi4A9n5os65c';
-  
-    useEffect(() => {
-      if (map.current) return; // stops map from intializing more than once
-  
-      map.current = new maplibregl.Map({
-        container: mapContainer.current,
-        style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${API_KEY}`,
-        center: [lng, lat],
-        zoom: zoom
+
+import MapView from "@arcgis/core/views/MapView";
+import Map from "@arcgis/core/Map";
+
+const MapComponent = () => {
+
+  const mapDiv = useRef(null);
+
+  useEffect(() => {
+    if (mapDiv.current) {
+      /**
+       * Initialize application
+       */
+      const webmap = new Map({
+        basemap: "dark-gray-vector"
       });
-  
-    }, [API_KEY, lng, lat, zoom]);
-  
-    return (
-      <div className="map-wrap">
-        <div ref={mapContainer} className="map" />
-      </div>
-    );
-  }
+
+      const view = new MapView({
+        container: mapDiv.current, // The id or node representing the DOM element containing the view.
+        map: webmap, // An instance of a Map object to display in the view.
+        center: [-117.1490,32.7353],
+        scale: 10000000 // Represents the map scale at the center of the view.
+      });
+
+      return () => view && view.destroy()
+
+    }
+  }, []);
+
+  return <div className="mapDiv" ref={mapDiv} style={{height: '100vh', width: "100%"}}></div>;
+}
+
+export default MapComponent;
